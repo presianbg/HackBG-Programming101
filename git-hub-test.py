8dffcbfbcfb9e0dbd186496d714b4ccec9581326
presianbg <presianbg@gmail.com>
2015-03-27 10:09:19 +0200

    Testing GitGutter

--- git-hub-test.py
+++ git-hub-test.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 
+
 def albala():
     print ("Test For Git-Hub")
     print ("Test N2 for githHub push")
     print ("Test N3 for githHub push - SSH ")
-
+    print ("Test N3 for githHub push - GitGutter ")
 
 
 if __name__ == '__main__':
