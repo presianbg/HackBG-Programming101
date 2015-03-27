d4daff982b4ebd66483f26a39fe66eaba8477b69
presianbg <presianbg@gmail.com>
2015-03-27 14:11:02 +0200

    WEEK2-2 3rd Task

---
+++ WEEK-2/2/generate_numbers.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+from random import randint
+import sys
+import os.path
+
+
+def generate_numbers(f_name, n):
+    with open(f_name, "w") as f:
+        for i in range(n):
+            f.write('%s ' % randint(1, 10000))
+        f.write('\n')
+
+
+if __name__ == '__main__':
+    f_name = os.path.normpath(sys.argv[1])
+    n = int(sys.argv[2])
+    generate_numbers(f_name, n)
