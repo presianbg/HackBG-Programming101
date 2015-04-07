76cc957caad5a0c442c9fab70e2e6cb61ae9ccec
presianbg <presianbg@gmail.com>
2015-04-07 18:13:03 +0300

    Getting Better in Playlist

--- WEEK-4  A Music Library/playlist_class.py
+++ WEEK-4  A Music Library/playlist_class.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 from songs_class import Songs
 import datetime
+import copy
+import random
+from tabulate import tabulate
 
 
 class Playlist():
@@ -10,6 +13,8 @@ class Playlist():
         self.repeat = repeat
         self.shuffle = shuffle
         self.rocklist = []
+        self.song_number = 0
+        self.first_random = True
 
     def is_a_song(self, song):
         if isinstance(song, Songs):
@@ -33,3 +38,71 @@ class Playlist():
         for song in self.rocklist:
             self.total_secs += song.get_lenght(seconds=True)
         return str(datetime.timedelta(seconds=self.total_secs))
+
+    def get_artists(self):
+        self.artista = {}
+        for songs in self.rocklist:
+            if songs.artist in self.artista:
+                self.artista[songs.artist] += 1
+            else:
+                self.artista[songs.artist] = 1
+        return self.artista
+
+    def next_song(self):
+        if self.shuffle is False:
+            if self.song_number < len(self.rocklist):
+                self.current_song = self.rocklist[self.song_number]
+                self.song_number += 1
+                return self.current_song
+            elif self.repeat is True and self.song_number == len(self.rocklist):
+                self.song_number = 0
+                self.current_song = self.rocklist[self.song_number]
+                self.song_number += 1
+                return self.current_song
+            else:
+                raise NoMoreSongsInPlaylist
+
+        elif self.shuffle is True:
+            if self.first_random is True:
+                self.random_list = copy.deepcopy(self.rocklist)
+                self.first_random = False
+
+            if len(self.random_list) == 0 and self.repeat is False:
+                raise NoMoreSongsInPlaylist
+            elif len(self.random_list) == 0 and self.repeat and not self.first_random:
+                self.random_list = copy.deepcopy(self.rocklist)
+
+            if len(self.random_list) > 0:
+                self.current_song = random.choice(self.random_list)
+                self.random_list.remove(self.current_song)
+
+        return self.current_song
+
+    def pprint_playlist(self):
+        self.pptable = []
+        self.headers = ["Artist", "Album", "SongTitle", "Lenght"]
+        for song in self.rocklist:
+            self.pptable.append([song.artist, song.album, song.title, song.lenght])
+        print (tabulate(self.pptable, self.headers, tablefmt="fancy_grid"))
+
+
+class NoMoreSongsInPlaylist(Exception):
+    pass
+
+
+
+def main():
+    rock_song = Songs(title="Shackler's Revenge", artist="Guns N' Roses", album="Chinese Democracy", lenght="4:28")
+    roll_song = Songs(title="Final Masquerade", artist="Linkin Park", album="The Hunting Party", lenght="3:34")
+    rock_rolla = Songs(title="Knocking On Heaven's Door", artist="Guns N' Roses", album="Live concert", lenght="5:22")
+    musiclist = Playlist(repeat=True, shuffle=True)
+    musiclist.add_song(rock_song)
+    musiclist.add_song(roll_song)
+    musiclist.add_song(rock_rolla)
+    musiclist.pprint_playlist()
+    print(str(musiclist.next_song()))
+    print(str(musiclist.next_song()))
+    print(str(musiclist.next_song()))
+
+if __name__ == '__main__':
+    main()
--- WEEK-4  A Music Library/test_playlist.py
+++ WEEK-4  A Music Library/test_playlist.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import unittest
-from playlist_class import Playlist
+from playlist_class import Playlist, NoMoreSongsInPlaylist
 from songs_class import Songs
 
 class TestingPlaylist(unittest.TestCase):
@@ -8,8 +8,10 @@ class TestingPlaylist(unittest.TestCase):
     def setUp(self):
         self.rock_song = Songs(title="Shackler's Revenge", artist="Guns N' Roses", album="Chinese Democracy", lenght="4:28")
         self.roll_song = Songs(title="Final Masquerade", artist="Linkin Park", album="The Hunting Party", lenght="3:34")
+        self.rock_rolla = Songs(title="Knocking On Heaven's Door", artist="Guns N' Roses", album="Live concert", lenght="5:22")
         self.musiclist = Playlist()
         self.musiclist.add_song(self.rock_song)
+        self.musiclist.add_song(self.roll_song)
         self.ne_e_pesen = 0
 
     def test_add_song(self):
@@ -24,11 +26,46 @@ class TestingPlaylist(unittest.TestCase):
         self.assertNotIn(self.rock_song, self.musiclist.rocklist)
 
     def test_get_total_lenght(self):
-        self.musiclist.add_song(self.roll_song)
         self.assertEqual(self.musiclist.get_total_lenght(), "0:08:02")
 
-    def test_artist_hist(sefl):
-        pass
+    def test_artists_hist(self):
+        self.musiclist.add_song(self.rock_rolla)
+        artists = self.musiclist.get_artists()
+        self.assertEqual(artists["Guns N' Roses"], 2)
+
+    def test_next_song(self):
+        self.assertEqual(self.musiclist.next_song(), self.rock_song)
+        self.assertEqual(self.musiclist.next_song(), self.roll_song)
+        with self.assertRaises(NoMoreSongsInPlaylist):
+            self.musiclist.next_song()
+
+    def test_next_song_repat(self):
+        self.repeat_mlist = Playlist(repeat=True)
+        self.repeat_mlist.add_song(self.rock_song)
+        self.repeat_mlist.add_song(self.roll_song)
+        self.repeat_mlist.add_song(self.rock_rolla)
+        self.assertEqual(self.repeat_mlist.next_song(), self.rock_song)
+        self.assertEqual(self.repeat_mlist.next_song(), self.roll_song)
+        self.assertEqual(self.repeat_mlist.next_song(), self.rock_rolla)
+        self.assertEqual(self.repeat_mlist.next_song(), self.rock_song)
+
+    def test_next_song_random(self):
+        self.shuffle_mlist = Playlist(shuffle=True)
+        self.shuffle_mlist.add_song(self.rock_song)
+        self.shuffle_mlist.add_song(self.roll_song)
+        with self.assertRaises(NoMoreSongsInPlaylist):
+            self.shuffle_mlist.next_song()
+            self.shuffle_mlist.next_song()
+            self.shuffle_mlist.next_song()
+
+    def test_next_song_random_repat(self):
+        self.repeat_mlist = Playlist(repeat=True, shuffle=True)
+        self.repeat_mlist.add_song(self.rock_song)
+        self.repeat_mlist.add_song(self.roll_song)
+        self.repeat_mlist.add_song(self.rock_rolla)
+        for i in range(25):
+            self.assertIsInstance(self.repeat_mlist.next_song(), Songs)
+
 
 if __name__ == '__main__':
     unittest.main()
